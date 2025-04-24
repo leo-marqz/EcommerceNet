54ff19f93ba8109719bbe6e406771054e438dc67
Elmer Marquez <leomarqz2020@gmail.com>
2025-04-23 23:58:33 -0600

    Definiendo la clase EcommerceApplicationDbContext

---
+++ Ecommerce.Infrastructure/Persistence/EcommerceApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿
+
+using Ecommerce.Domain.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Infrastructure.Persistence
+{
+    public class EcommerceApplicationDbContext : IdentityDbContext<User>
+    {
+        public EcommerceApplicationDbContext(DbContextOptions<EcommerceApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
