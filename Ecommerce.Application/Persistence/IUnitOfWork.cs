5767f2c506d770de950d0ce87c3344f9225a7e22
Elmer Marquez <leomarqz2020@gmail.com>
2025-04-26 20:33:20 -0600

    Se estan configurando la interface de Repositorio generico y de Unidad de Trabajo o UnitOfWork

---
+++ Ecommerce.Application/Persistence/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Application.Persistence
+{
+    //Its main mission: to coordinate multiple database operations
+    //so that they are all stored together(or not at all, if something goes wrong).
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepositoryAsync<TEntity> Repository<TEntity>() where TEntity : class;
+        Task<int> ApplyChangesAsync();
+    }
+}
