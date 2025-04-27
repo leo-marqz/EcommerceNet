88ad33d2b5f823bd4ebf6f2f8510569ad8ed6d6e
Elmer Marquez <leomarqz2020@gmail.com>
2025-04-26 21:14:40 -0600

    Se configuro el repositorio base o generico y la unidad de trabajo o unitofwork.

---
+++ Ecommerce.Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,55 @@
+﻿
+using Ecommerce.Application.Persistence;
+using Ecommerce.Infrastructure.Persistence;
+using System;
+using System.Collections;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Infrastructure.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private Hashtable? _repositories;
+        private readonly EcommerceApplicationDbContext _context;
+
+        public UnitOfWork(EcommerceApplicationDbContext context)
+        {
+            _context = context;   
+        }
+
+        public async Task<int> ApplyChangesAsync()
+        {
+            try
+            {
+                return await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error en la transaccion (uow): ", ex);
+            }
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        public IRepositoryAsync<TEntity> Repository<TEntity>() where TEntity : class
+        {
+            if(_repositories is null) _repositories = new Hashtable();
+
+            var type = typeof(TEntity).Name;
+
+            if (!_repositories.ContainsKey(type))
+            {
+                var repositoryType = typeof(BaseRepositoryAsync<>);
+                var repositoryInstance = Activator.CreateInstance(
+                        type: repositoryType.MakeGenericType( typeof(TEntity) ), 
+                        _context
+                    );
+                _repositories.Add( type, repositoryInstance );
+            }
+            return (IRepositoryAsync<TEntity>)_repositories[type]!;
+        }
+    }
+}
