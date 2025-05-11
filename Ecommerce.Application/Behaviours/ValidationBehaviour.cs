9a1708ecce4802e2f4628ab1b3b1459e0539dfa2
Elmer Marquez <leomarqz2020@gmail.com>
2025-05-10 22:15:10 -0600

    recolector de errores en casos donde se dispare un validador

---
+++ Ecommerce.Application/Behaviours/ValidationBehaviour.cs
@@ -0,0 +1,51 @@
+﻿using FluentValidation;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Application.Behaviours
+{
+    public class ValidationBehaviour<TRequest, TResponse>
+        : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public ValidationBehaviour(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators ?? throw new ArgumentNullException(nameof(validators));
+        }
+
+        public async Task<TResponse> Handle(
+                TRequest request, 
+                RequestHandlerDelegate<TResponse> next, 
+                CancellationToken cancellationToken
+            )
+        {
+            if (_validators.Any())
+            {
+                var context = new ValidationContext<TRequest>(request);
+                var validatonResults = await Task.WhenAll(
+                    _validators.Select(v => v.ValidateAsync(context, cancellationToken))
+                );
+                var failures = validatonResults
+                    .SelectMany(result => result.Errors)
+                    .Where(f => f != null)
+                    .ToList();
+
+                if (failures.Count != 0)
+                {
+                    throw new ValidationException(
+                        $"Command Validation Errors for type {typeof(TRequest).Name}",
+                        failures
+                    );
+                }
+            }
+
+            return await next();
+
+        }
+    }
+}
