93c9a965c96f460e1eefee2063e665e16aaa2a86
Elmer Marquez <leomarqz2020@gmail.com>
2025-05-13 17:10:05 -0600

    Se configuro el automapper

---
+++ Ecommerce.Application/Mappings/MappingProfile.cs
@@ -0,0 +1,13 @@
+﻿
+using AutoMapper;
+
+namespace Ecommerce.Application.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            
+        }
+    }
+}
